53e75870a4b8f8827d4085f2c3c726fecdf3570a
Sophia Chan <ceasings@gmail.com>
2018-10-13 14:40:29 -0700

    added colours

--- src/treemap.R
+++ src/treemap.R
@@ -6,39 +6,47 @@ source("src/data_clean_up.R")
 # Select data for a specific year, for both sexes and all ages
 data <- data_top_category_filtered %>%
   filter(
-    Sex=="Both sexes", 
     `Age at time of death`=="all ages", 
-    Characteristics=="Number of deaths", 
+    Characteristics=="Number of deaths",
+    Sex=="Both sexes",
     REF_DATE==2000)
 
-# Assign an ID to each cause of death
-data$cause_id <- data %>% group_indices(`Cause of death (ICD-10)`)
+# Add one to each value so that we never have zero
+data <- rename(data, cause=`Cause of death (ICD-10)`) %>%
+  mutate(value_plus=VALUE+1)
+
+# Assign a unique index for each cause  
+data$cause_id <- data %>% group_indices(cause)
 
 # Select a specific number of columns
 tree_data <- data %>%
-  select(`Cause of death (ICD-10)`, Characteristics, VALUE, cause_id)
+  select(cause, Characteristics, value_plus, cause_id, Sex, REF_DATE)
 
 # Create a new row for the parent
 # https://stackoverflow.com/questions/44399496/rstudio-treemap-idvar-does-not-match-parentvar
 tree_data_add <- tibble(
-  `Cause of death (ICD-10)`=c("Number of deaths"),
+  cause=c("Number of deaths"),
   Characteristics=c(NA),
-  VALUE=sum(tree_data$VALUE),
-  cause_id=c(NA)
+  value_plus=sum(tree_data$value_plus),
+  cause_id=c(NA),
+  Sex=c(NA),
+  REF_DATE=c(NA)
 )
 
 # Join the dataframes
-tree_data <- rbind(tree_data, tree_data_add) %>%
-  filter(VALUE > 0)
+tree_data <- rbind(tree_data, tree_data_add)
 
 # Plot!
-tree <-
-  gvisTreeMap(
-    tree_data,
-    "Cause of death (ICD-10)",
-    "Characteristics",
-    "VALUE",
-    "cause_id"
-  )
-
-plot(tree)
+gvisTreeMap(
+  tree_data,
+  "cause",
+  "Characteristics",
+  "value_plus",
+  "cause_id",
+  options=list(
+    minColor='#ee0979',
+    # midColor='#D76D77',
+    maxColor='#ff6a00',
+    highlightOnMouseOver=TRUE
+    )
+  ) %>% plot()
