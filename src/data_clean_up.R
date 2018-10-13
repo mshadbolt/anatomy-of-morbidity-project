d33c9784bc070dfda924e89da28051c3f0dd520d
Katarina <kat9234@gmail.com>
2018-10-13 11:32:23 -0700

    Added library loading. Separated out selecting by column, filtering by category

--- src/data_clean_up.R
+++ src/data_clean_up.R
@@ -1,13 +1,18 @@
+library(tidyverse)
+
 #Read in data
 data_deaths_and_age_specific_mortality <- read_csv("data/13100392.csv")
 
 #Get top categories
 categories=read.delim("resources/categories.txt", header = FALSE)
 
-#Filter to include only data in top categories, and only necessary columns
-data_top_category_filtered = filter(data_deaths_and_age_specific_mortality, `Cause of death (ICD-10)` %in% categories$V1) %>%
+#Select only relevant columns
+data_relevant_columns = data_deaths_and_age_specific_mortality %>%
   select(REF_DATE, `Age at time of death`, Sex, `Cause of death (ICD-10)`, Characteristics, VALUE) %>%
   mutate(`Age at time of death`= str_sub(`Age at time of death`,23))
+  
+#Filter to include only data in top categories
+data_top_category_filtered = filter(data_relevant_columns, `Cause of death (ICD-10)` %in% categories$V1) 
 
 
 
