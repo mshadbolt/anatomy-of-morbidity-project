a4603971786d3c95153cc42696a405417858f246
Marion Shadbolt <marion.shadbolt@gmail.com>
2018-10-13 13:22:56 -0700

    published to web and minor cleanups

--- shiny_template/shiny_tab_template/server.R
+++ shiny_template/shiny_tab_template/server.R
@@ -27,6 +27,46 @@ server <- function(input, output) {
             )
   })
   
+  output$plot_male <- renderPlot({
+    year_input <- input$yearMF
+    sex_input <-  "Males"
+    age_input <-  input$ageMF
+    
+    filtered_cause_of_death_rates <- cause_of_death_rates %>%
+      filter(year == year_input,
+             Sex %in% sex_input,
+             age_factor == age_input)
+    
+    treemap(filtered_cause_of_death_rates, #Your data frame object
+            index="cause",  #A list of your categorical variables
+            vSize = "rate_per_100k",  #This is your quantitative variable
+            type="index", #Type sets the organization and color scheme of your treemap
+            palette = "Blues",  #Select your color palette from the RColorBrewer presets or make your own.
+            title="Male leading causes of death", #Customize your title
+            fontsize.title = 14 #Change the font size of the title
+    )
+  })
+  
+  output$plot_female <- renderPlot({
+    year_input <- input$yearMF
+    sex_input <-  "Females"
+    age_input <-  input$ageMF
+    
+    filtered_cause_of_death_rates <- cause_of_death_rates %>%
+      filter(year == year_input,
+             Sex %in% sex_input,
+             age_factor == age_input)
+    
+    treemap(filtered_cause_of_death_rates, #Your data frame object
+            index="cause",  #A list of your categorical variables
+            vSize = "rate_per_100k",  #This is your quantitative variable
+            type="index", #Type sets the organization and color scheme of your treemap
+            palette = "PuRd",  #Select your color palette from the RColorBrewer presets or make your own.
+            title="Female leading causes of death", #Customize your title
+            fontsize.title = 14 #Change the font size of the title
+    )
+  })
+  
   # Generate a summary of the data ----
   # output$summary <- renderPrint({
   #   summary(d())
--- shiny_template/shiny_tab_template/ui.R
+++ shiny_template/shiny_tab_template/ui.R
@@ -20,28 +20,45 @@ ui <- fluidPage(
                                     choices = list("Male" = "Males",
                                                    "Female" = "Females"),
                                     selected = "Males"),
-                 
                  # br() element to introduce extra vertical spacing ----
                  br(),
-                 
                  selectInput(inputId = "age", 
                              label = "Age category", 
                              choices = levels(cause_of_death_rates$age_factor), 
                              selected = NULL, multiple = FALSE,
                              selectize = TRUE, width = NULL, size = NULL),
                  # Input: Slider for the number of observations to generate ----
+                 br(),
                  sliderInput(inputId = "year",
                              label = "Year:",
                              value = 2000,
                              min = min(cause_of_death_rates$year),
                              max = max(cause_of_death_rates$year), 
-                             step = 1),
-                 br()
-               ),
-               mainPanel(
-                 plotOutput("plot_cause")
+                             step = 1)
+                 ),
+                 mainPanel(
+                   plotOutput("plot_cause")
                )
             )),
+    tabPanel("Males vs Females", 
+             sidebarLayout(
+               sidebarPanel(selectInput(inputId = "ageMF", 
+                                        label = "Age category", 
+                                        choices = levels(cause_of_death_rates$age_factor), 
+                                        selected = NULL, multiple = FALSE,
+                                        selectize = TRUE, width = NULL, size = NULL),
+                            # Input: Slider for the number of observations to generate ----
+                            br(),
+                            sliderInput(inputId = "yearMF",
+                                        label = "Year:",
+                                        value = 2000,
+                                        min = min(cause_of_death_rates$year),
+                                        max = max(cause_of_death_rates$year), 
+                                        step = 1)
+               ),
+               mainPanel(plotOutput("plot_male"),
+                         plotOutput("plot_female"))
+             )),
     tabPanel("When we die", 
              sidebarLayout(
                sidebarPanel(),
