c1c63aab37ea37d9e2fd354010ba86788149e4c9
Marion Shadbolt <marion.shadbolt@gmail.com>
2018-10-13 12:59:29 -0700

    made sidepanel specific to tab

--- shiny_template/shiny_tab_template/ui.R
+++ shiny_template/shiny_tab_template/ui.R
@@ -6,41 +6,87 @@ library(shiny)
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("Tabsets"),
+  titlePanel("The anatomy of Morbidity"),
   
+  tabsetPanel(
+    type = "tabs",
+    tabPanel("How we die",
+             sidebarLayout(
+               
+               sidebarPanel(
+                 # Input: Select the random distribution type ----
+                 checkboxGroupInput(inputId = "sex", 
+                                    label = "Sex:",
+                                    choices = list("Male" = "Males",
+                                                   "Female" = "Females"),
+                                    selected = "Males"),
+                 
+                 # br() element to introduce extra vertical spacing ----
+                 br(),
+                 
+                 selectInput(inputId = "age", 
+                             label = "Age category", 
+                             choices = levels(cause_of_death_rates$age_factor), 
+                             selected = NULL, multiple = FALSE,
+                             selectize = TRUE, width = NULL, size = NULL),
+                 # Input: Slider for the number of observations to generate ----
+                 sliderInput(inputId = "year",
+                             label = "Year:",
+                             value = 2000,
+                             min = min(cause_of_death_rates$year),
+                             max = max(cause_of_death_rates$year), 
+                             step = 1),
+                 br()
+               ),
+               mainPanel(
+                 plotOutput("plot_cause")
+               )
+            )),
+    tabPanel("When we die", 
+             sidebarLayout(
+               sidebarPanel(),
+               mainPanel(plotOutput("plot_expec"))
+               )),
+    tabPanel("Where we die", 
+             sidebarLayout(
+               sidebarPanel(),
+               mainPanel(plotOutput("plot_map")))) #,
+    #tabPanel("Summary", verbatimTextOutput("summary")),
+    #tabPanel("Table", tableOutput("table")
+  )
   # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Select the random distribution type ----
-      checkboxGroupInput(inputId = "sex", 
-                   label = "Sex:",
-                   choices = list("Male" = "Males",
-                                  "Female" = "Females"),
-                   selected = "Males"),
-      
-      # br() element to introduce extra vertical spacing ----
-      br(),
-      
-      selectInput(inputId = "age", 
-                  label = "Age category", 
-                  choices = levels(cause_of_death_rates$age_factor), 
-                  selected = NULL, multiple = FALSE,
-                  selectize = TRUE, width = NULL, size = NULL)
-      ,
-      
-      # Input: Slider for the number of observations to generate ----
-      sliderInput(inputId = "year",
-                  label = "Year:",
-                  value = 2000,
-                  min = min(cause_of_death_rates$year),
-                  max = max(cause_of_death_rates$year), 
-                  step = 1),
-      
-      br() #,
-      
+  # sidebarLayout(
+  #   
+  #   # Sidebar panel for inputs ----
+  #   sidebarPanel(
+  #     
+  #     # Input: Select the random distribution type ----
+  #     checkboxGroupInput(inputId = "sex", 
+  #                  label = "Sex:",
+  #                  choices = list("Male" = "Males",
+  #                                 "Female" = "Females"),
+  #                  selected = "Males"),
+  #     
+  #     # br() element to introduce extra vertical spacing ----
+  #     br(),
+  #     
+  #     selectInput(inputId = "age", 
+  #                 label = "Age category", 
+  #                 choices = levels(cause_of_death_rates$age_factor), 
+  #                 selected = NULL, multiple = FALSE,
+  #                 selectize = TRUE, width = NULL, size = NULL)
+  #     ,
+  #     
+  #     # Input: Slider for the number of observations to generate ----
+  #     sliderInput(inputId = "year",
+  #                 label = "Year:",
+  #                 value = 2000,
+  #                 min = min(cause_of_death_rates$year),
+  #                 max = max(cause_of_death_rates$year), 
+  #                 step = 1),
+  #     
+  #     br() #,
+  #     
       # sliderInput(inputId = "age",
       #             label = "Age:",
       #             value = 25,
@@ -48,20 +94,20 @@ ui <- fluidPage(
       #             max = max(cause_of_death_rates$), 
       #             step = 1)
       
-    ),
+ #   ),
     
     # Main panel for displaying outputs ----
-    mainPanel(
+    # mainPanel(
+    #   
+    #   # Output: Tabset w/ plot, summary, and table ----
+    #   tabsetPanel(type = "tabs",
+    #               tabPanel("How we die", plotOutput("plot_cause")),
+    #               tabPanel("When we die", plotOutput("plot_expec")),
+    #               tabPanel("Where we die", plotOutput("plot_map")),
+    #               tabPanel("Summary", verbatimTextOutput("summary")) #,
+    #               #tabPanel("Table", tableOutput("table"))
+    #   )
       
-      # Output: Tabset w/ plot, summary, and table ----
-      tabsetPanel(type = "tabs",
-                  tabPanel("How we die", plotOutput("plot_cause")),
-                  tabPanel("When we die", plotOutput("plot_expec")),
-                  tabPanel("Where we die", plotOutput("plot_map")),
-                  tabPanel("Summary", verbatimTextOutput("summary")) #,
-                  #tabPanel("Table", tableOutput("table"))
-      )
-      
-    )
-  )
+    #)
+#  )
 )
