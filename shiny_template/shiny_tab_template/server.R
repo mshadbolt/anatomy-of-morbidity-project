5a2b10b21c04f7237d8491495bc6b72e6ce119d4
Katarina <kat9234@gmail.com>
2018-10-14 12:25:24 -0700

    Clean up for easier testing2

--- shiny_template/shiny_tab_template/server.R
+++ shiny_template/shiny_tab_template/server.R
@@ -72,54 +72,6 @@ server <- function(input, output) {
       )
       )
   }
-
-  
-    
-  output$plot_cause_new <- renderGvis({
-    my_func(input$sex)
-    # year_input <- input$year
-    # sex_input <- input$sex
-    # age_input <- input$age
-    # 
-    # filtered_tree_data <- tree_data %>%
-    #   filter(year == year_input,
-    #          Sex == sex_input,
-    #          age_factor == age_input)
-    # 
-    # # Create a new row for the parent
-    # # https://stackoverflow.com/questions/44399496/rstudio-treemap-idvar-does-not-match-parentvar
-    # 
-    # filtered_tree_data_add <- tibble(
-    #   cause=c("Number of deaths"),
-    #   Characteristics=c(NA),
-    #   value_plus=c(1), #sum(tree_data$value_plus),
-    #   cause_id=c(NA),
-    #   Sex=c(NA),
-    #   year=c(NA),
-    #   age_factor = c(NA),
-    #   total_deaths = c(NA)
-    # )
-    # 
-    # # Join the dataframes
-    # filtered_tree_data <- bind_rows(filtered_tree_data, filtered_tree_data_add)
-    # 
-    # gvisTreeMap(
-    #   filtered_tree_data,
-    #   "cause",
-    #   "Characteristics",
-    #   "value_plus",
-    #   "cause_id",
-    #   options=list(
-    #     minColor='#ee0979',
-    #     # midColor='#D76D77',
-    #     maxColor='#ff6a00',
-    #     highlightOnMouseOver=TRUE,
-    #     generateTooltip = "function(row, size, value) { 
-    #                           return '<div style=\"background:#fd9; padding:10px; border-style:solid\">' + data.getValue(row, 3) + '</div>'; 
-    # }"
-    #   )
-    # )
-  })
   
   output$death_total <- renderText({
     year_input <- input$year
@@ -137,77 +89,17 @@ server <- function(input, output) {
     paste("Total number of deaths: <b>", death_total,"</b>", sep = " ")
   })
 
+  
+  output$plot_cause_new <- renderGvis({
+    my_func(input$sex)
+  })
+  
   output$plot_male1 <-renderGvis({
     my_func("Males")
   })
   
   output$plot_female1 <-renderGvis({
     my_func("Females")
-  })
-  
-  output$plot_male <-renderGvis({
-      year_input <- input$year
-      sex_input <-  "Males"
-      age_input <-  input$age
-    
-    filtered_tree_data <- tree_data %>%
-      filter(year == year_input,
-             Sex == sex_input,
-             age_factor == age_input)
-    
-    # Create a new row for the parent
-    # https://stackoverflow.com/questions/44399496/rstudio-treemap-idvar-does-not-match-parentvar
-    
-    filtered_tree_data_add <- tibble(
-      cause=c("Number of deaths"),
-      Characteristics=c(NA),
-      value_plus=c(1),
-      cause_id=c(NA),
-      Sex=c(NA),
-      year=c(NA),
-      age_factor = c(NA),
-      total_deaths = c(NA)
-    )
-    
-    # Join the dataframes
-    filtered_tree_data <- bind_rows(filtered_tree_data, filtered_tree_data_add)
-    
-    gvisTreeMap(
-      filtered_tree_data,
-      "cause",
-      "Characteristics",
-      "value_plus",
-      "cause_id",
-      options=list(
-        minColor='#ee0979',
-        # midColor='#D76D77',
-        maxColor='#ff6a00',
-        highlightOnMouseOver=TRUE,
-        generateTooltip = "function(row, size, value) { 
-                              return '<div style=\"background:#fd9; padding:10px; border-style:solid\">' + data.getValue(row, 3) + '</div>'; 
-    }"
-      )
-    )
-  })
-
-  output$plot_male <- renderPlot({
-    year_input <- input$year
-    sex_input <-  "Males"
-    age_input <-  input$age
-    
-    filtered_cause_of_death_rates <- cause_of_death_rates %>%
-      filter(year == year_input,
-             Sex %in% sex_input,
-             age_factor == age_input)
-    
-    treemap(filtered_cause_of_death_rates, #Your data frame object
-            index="cause",  #A list of your categorical variables
-            vSize = "rate_per_100k",  #This is your quantitative variable
-            type="index", #Type sets the organization and color scheme of your treemap
-            palette = "Blues",  #Select your color palette from the RColorBrewer presets or make your own.
-            title="Male leading causes of death", #Customize your title
-            fontsize.title = 14 #Change the font size of the title
-    )
   })
   
   output$plot_female <- renderPlot({
--- shiny_template/shiny_tab_template/ui.R
+++ shiny_template/shiny_tab_template/ui.R
@@ -69,7 +69,6 @@ ui <- fluidPage(
                ),
                mainPanel(htmlOutput("plot_male1"),
                          htmlOutput("plot_female1"),
-                         plotOutput("plot_male"),
                          plotOutput("plot_female"),
                          p("This visualisation was created using a derivative of:  Table 13-10-0392 Deaths and age-specific mortality rates, by selected grouped causes"))
              )),
